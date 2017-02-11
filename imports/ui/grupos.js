3042a6ef2f15f6e19ad8fe320fdeac4b59d51b2b
Alexsandro Specht <alexsandrospecht@gmail.com>
2017-02-11 11:41:33 -0200

    Adicionando visibilidade aos grupos

--- imports/ui/grupos.js
+++ imports/ui/grupos.js
@@ -4,32 +4,48 @@ import { Template } from 'meteor/templating';
 import { Grupos } from '../api/grupos.js';
 
 Template.meusGrupos.helpers({
-  meusGrupos() {
+	meusGrupos() {
 		return Grupos.find( {usuario: Meteor.userId()}, { sort: { nome: 1 } });
 	},
 });
 
-Template.novoGrupo.events({		
-  'submit #form-grupo'(event) {
-    event.preventDefault();
- 
-    const target = event.target;
-    const text = target.text.value;
- 
-    Grupos.insert({
-      nome: text,
-      criadoEm: new Date(),
-      usuario: Meteor.userId()
-    });
-
-    target.text.value = '';
-    alert('Novo Grupo Cadastrado Com Sucesso!');
-    BlazeLayout.render("meusGrupos");
-  },
+Template.novoGrupo.onRendered(function () {
+	$('#grupoPublico').prop('checked', true);
 });
 
-Template.meusGrupos.events({		
-  'click #criarGrupo': function(event) {
-    BlazeLayout.render("novoGrupo");
-   },
+Template.novoGrupo.events({		
+	'submit #form-grupo'(event) {
+		event.preventDefault();
+
+		var public = $("#grupoPublico").is(":checked");
+
+		const target = event.target;
+		const text = target.text.value;
+
+		Grupos.insert({
+		  nome: text,
+		  criadoEm: new Date(),
+		  publico: public,
+		  usuario: Meteor.userId()
+		});
+
+		target.text.value = '';
+		alert('Novo Grupo Cadastrado Com Sucesso!');
+		BlazeLayout.render("meusGrupos");
+	},
+	'click #cancelarGrupo' (event) {
+	    BlazeLayout.render("meusGrupos");
+	},
+	'change #grupoPrivado' (event) {
+		$('#grupoPublico').prop('checked', false);
+	},
+	'change #grupoPublico' (event) {
+		$('#grupoPrivado').prop('checked', false);
+	},		
+});
+
+Template.meusGrupos.events({	
+	'click #criarGrupo': function(event) {
+		BlazeLayout.render("novoGrupo");
+	},
 });
