f64b0fd4e0713c7438c1565083fb35f3c34e4213
Maicon Michelon <maicon.f.michelon@gmail.com>
2017-02-20 11:52:26 -0300

    melhorias na tela de avaliacao

--- imports/ui/avaliar.js
+++ imports/ui/avaliar.js
@@ -30,32 +30,79 @@ Template.avaliar.helpers({
   },
 });
 
+Template.campoAvaliacao.helpers({
+  estrelas() {
+    return [1, 2, 3, 4, 5];
+  },
+  checked(pos, nota) {
+    return pos === nota;
+  }
+});
+
 Template.avaliar.events({
-  'submit .nova-avaliacao'(event) {
-    event.preventDefault();
-  
-    avaliacao = new Object();
-
-    const target = event.target;
-
-    avaliacao.notaPasse = parseInt(target.notaPasse.value);
-    avaliacao.notaDrible = parseInt(target.notaDrible.value);
-    avaliacao.notaPreparoFisico = parseInt(target.notaPreparoFisico.value);
-    avaliacao.notaChute = parseInt(target.notaChute.value);
-    avaliacao.notaMarcacao = parseInt(target.notaMarcacao.value);
-    avaliacao.avaliadoId = getAvaliadoId();
-
-    grupoDefault = Grupos.findOne();
-
-    Meteor.call('avaliacoes.insertOrUpdate', grupoDefault._id, avaliacao);
-
-    params = {
-      grupo: FlowRouter.current().params.grupo_id,
-    };
-
-    FlowRouter.go('/grupos/:grupo/atletas', params);
+  'mouseenter .review > label'(event) {
+    toggleLabel(event);
+  },
+  'mouseleave .review > label' (event) {
+    untoggleLabels(event);
+  },
+  'click .review > label' (event) {
+    checkRadio(event);
   },
 });
+
+function untoggleLabels(event) {
+  radioInput = getRadioInput(event.target);
+  if (radioInput != null) {
+    checkedRadio = radioInput.parent().children('input.toggled');
+
+    radioInput.parent().children('label').removeClass('toggled');
+    checkedRadio.prevAll('label').addClass('toggled');
+  }
+}
+
+function toggleLabel(event) {
+  radioInput = getRadioInput(event.target);
+  if (radioInput != null) {
+    radioInput.parent().children('label').removeClass('toggled');
+    radioInput.prevAll('label').addClass('toggled');
+  }
+}
+
+function checkRadio(event) {
+  radioInput = getRadioInput(event.target.parentElement);
+  if (radioInput == null) {
+    return;
+  }
+
+  radioInput.parent().children('input').removeClass('toggled');
+  radioInput.addClass('toggled');
+  salvar();
+}
+
+function salvar() {
+  avaliacao = new Object();
+  avaliacao.notaPasse = parseInt($('input.toggled[name=notaPasse]')[0].value);
+  avaliacao.notaDrible = parseInt($('input.toggled[name=notaDrible]')[0].value);
+  avaliacao.notaPreparoFisico = parseInt($('input.toggled[name=notaPreparoFisico]')[0].value);
+  avaliacao.notaChute = parseInt($('input.toggled[name=notaChute]')[0].value);
+  avaliacao.notaMarcacao = parseInt($('input.toggled[name=notaMarcacao]')[0].value);
+  avaliacao.avaliadoId = getAvaliadoId();
+
+  grupoDefault = Grupos.findOne();
+
+  Meteor.call('avaliacoes.insertOrUpdate', grupoDefault._id, avaliacao);
+}
+
+function getRadioInput(label) {
+  forAttribute = label.attributes["for"];
+  if (forAttribute == undefined) {
+    return;
+  }
+
+  idInput = forAttribute.value;
+  return $('#' + idInput);
+}
 
 function getAvaliadoId() {
   return FlowRouter.current().params.user_id;
